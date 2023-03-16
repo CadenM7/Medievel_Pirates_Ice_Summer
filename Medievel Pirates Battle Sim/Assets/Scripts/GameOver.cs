9a98f8304eabea510902152bd1013e0336fbdf03
MJ1401 <johnsonmb@hendrix.edu>
2023-03-16 09:43:58 -0500

    Added Game Over Script

---
+++ Medievel Pirates Battle Sim/Assets/Scripts/GameOver.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+
+    public GameObject[] AllEnemies;
+    public GameObject NearestEnemy;
+    float distance;
+    float nearestDistance = 1000000;
+
+    public bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AllEnemies = GameObject.FindGameObjectsWithTag("Knight");
+        if (AllEnemies.Length == 0) {
+            gameOver = true;
+        }
+    }
+}
